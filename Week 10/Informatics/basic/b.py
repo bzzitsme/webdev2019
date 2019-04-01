bd21da3dcd5ae3008159ec95737312850dde1dfa
BzzItsMe <bzzitsme@gmail.com>
2019-04-01 20:57:50 +0600

    Informatics basic problems

---
+++ Week 10/Informatics/basic/b.py
@@ -0,0 +1,5 @@
+import math
+
+n = int(input())
+print('The next number for the number {} is {}.'.format(n, n + 1))
+print('The previous number for the number {} is {}.'.format(n, n - 1));
---
+++ Week 10/Informatics/basic/e.py
@@ -0,0 +1,3 @@
+n = int(input())
+k = int(input())
+print(int(109 + (n * k % 109)) % 109)
