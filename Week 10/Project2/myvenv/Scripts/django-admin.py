b879bc0f51b876e05ad0c89a9ffd502771b838b1
BzzItsMe <bzzitsme@gmail.com>
2019-04-02 03:43:03 +0600

    add lab

---
+++ Week 10/Project2/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\admin\desktop\web kbtu\webdev2019\week 10\project2\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
