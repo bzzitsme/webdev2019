fc7750b701a20fba57c8d45a2c3b1ee0b1020e91
BzzItsMe <bzzitsme@gmail.com>
2019-04-20 20:36:26 +0600

    Refresh

--- Week 10/Project1/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\admin\desktop\web kbtu\webdev2019\week 10\project1\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
