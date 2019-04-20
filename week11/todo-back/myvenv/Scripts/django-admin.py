b71f5c291c414bb64307a61d7f82f3af31bc13a0
BzzItsMe <bzzitsme@gmail.com>
2019-04-20 20:33:46 +0600

    add lab

--- week11/todo-back/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\admin\desktop\webdev2019\week11\todo-back\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
